71563a3ae6bb9921cff2141fc2ee9674ba215547
Daniel <danshaman05@gmail.com>
2024-02-21 22:41:41 +0100

    Fix issue with different tables for different day types.
    Getting of next departures (get_next_departures) works except of line 39 and similar lines.

---
+++ calendar_calculator.py
@@ -0,0 +1,81 @@
+
+from datetime import datetime, timedelta, date
+from workalendar.europe import Slovakia
+
+from imhd_scrapper import SCHEDULES_TABLE_IDS_DICT
+import constants
+def get_datetime_objects_between_2_dates(start_date: datetime, end_date: datetime):
+    lst = []
+    interval = timedelta(days=1)
+    current_date = start_date
+    while current_date <= end_date:
+        lst.append(current_date)
+        current_date += interval
+    return lst
+
+
+#Prazdniny 2024
+vianocne_first_day = datetime(2024, 1, 1)
+vianocne = [vianocne_first_day + timedelta(days=x) for x in range(7)]
+
+jarne_first_day = datetime(2024, 3, 4)
+jarne = [jarne_first_day + timedelta(days=x) for x in range(4)]
+
+velkonocne_first_day = datetime(2024, 3, 28)
+velkonocne_last_day = datetime(2024, 4, 2)
+velkonocne = get_datetime_objects_between_2_dates(velkonocne_first_day, velkonocne_last_day)
+
+letne_first_day = datetime(2024, 7, 1)
+letne_last_day =  datetime(2024, 8, 31)
+letne = get_datetime_objects_between_2_dates(letne_first_day, letne_last_day)
+
+# all school holidays
+skolske_prazdniny = vianocne + jarne + velkonocne + letne
+
+# Get current date but set it to midnight:
+today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+
+
+def calculate_day_for_imhd_schedules():
+    cal = Slovakia()
+    if cal.is_working_day(today):
+        if today in skolske_prazdniny:
+            return constants.WORKING_DAYS_SCHOOL_HOLIDAYS
+        else:
+             return constants.WORKING_DAYS_SCHOOL_YEAR
+    else:
+        return constants.FREE_DAYS
+
+
+
+"""Cele toto mozno nebude fungovat, lebo neviem ze ci linky DPB sa riadia iba podla letnych prazdnin, alebo aj podla
+ velkonocnych napriklad. Zrejme tam to zavisi vzdy od jednotlivej linky - od pismenok pri casoch. Kvoli tomu by bolo 
+ lepsie mozno fakt parsovat planovac cesty (https://imhd.sk/ba/planovac-cesty) a to tak, ze zadam jednu dalsiu zastavku danym smerom.
+ 
+ Je zaujimave, ze napr. linka 39 ma poriadok len pre pracovne a volne dni.
+ Linka 83 ma poriadok pre pracovne (skolsky rok) , pre pracovne dni (skol. prazdniny) a pre volne dni.
+ Linka 88 a 93 to maju rovnako ako 83.
+ 
+ Je tu vsak problem, ze Planovac trasy zobrazi vsetky linky, ktore idu na danu zastavku. Ja niekedy potrebujem ale len jednu linku - elektricku.
+ 
+ Mozem to poriesit tak, ze na stranke budem mat odkazy:
+ - elektricka 3 ( ukaze odchody z Jungmannovej)
+ - smer Dubravka - pouzije IMHD Planovac - ukaze busy 83, 84 ku Jarke
+ - smer autobusova stanica - pouzije IMHD Planovac - (linka 88, mozno aj elektricku ukaze)
+ 
+ Ale najprv by som skusil uz moj prvy navrh - pouzitim "custom" calendara. Treba však zistiť, či pre každú linku máme rovnaké názvy tabuliek a či sedia s prázdninami (SM14, SM9, ...)
+ 
+ 
+ Edited 21.2.2024:
+ Trebalo by aj testovat nejako toto. Jedna moznost je nacitat pre danu linku cestu k jednej najblizsej zastavke 
+ a porovnat s nasimi departures. Ma potom vyznam mat dva sposoby zistovania odchodov liniek? Mozno ma, 
+ pokial z planovaca vieme parsnut len malo liniek.
+ 
+Takto by som mohol spravit nejake testy, a tie by sa kazdy den spustali. Ak by nastal nejaky problem, tak na moj mail
+by sa poslalo varovanie, ze planovac nefunguje dobre. Tiez by sa to zobrazilo na stranke.
+
+Edited 12:50:
+I have found out, that "planovac trasy" will show only 4 next connections. If we want to show more, we need to click on "Neskôr".
+ """
+
+
--- imhd_scrapper.py
+++ imhd_scrapper.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
+import calendar_calculator
+import constants
 
 IMHD_URL_PREFIX = "https://imhd.sk"
 SCHEDULES_PAGE_URL = "https://imhd.sk/ba/cestovne-poriadky"
@@ -11,8 +13,13 @@ SCHEDULES_PAGE_URL_OFFLINE = "/home/daniel/PycharmProjects/My_assistant/imhd_off
 for schedules in this and the next hour only. Therefore, the number of schedules that programme outputs may be less than this number."""
 NEXT_SCHEDULES_COUNT = 6
 
+# ids for tables in IMHD schedules (e.g.: https://imhd.sk/ba/cestovny-poriadok/linka/3/Farsk%C3%A9ho/smer-Ra%C4%8Da/bd807c807f847f7f827c8275c18075b6858775be7f75c98075b3)
+SCHEDULES_TABLE_IDS_DICT = {constants.FREE_DAYS: "SM9",
+                            constants.WORKING_DAYS_SCHOOL_YEAR: "SM14",
+                            constants.WORKING_DAYS_SCHOOL_HOLIDAYS: "SM11"}
 
-# Slovnik: line = linka mestskej hromadnej dopravy (Bratislava)
+# EN/SK Dictionary:
+# line = linka mestskej hromadnej dopravy (Bratislava)
 
 def get_soup(url):
     # OLD APPROACH:
@@ -58,70 +65,90 @@ def get_line_schedules_url(line: int, direction: str, stop: str):
 
 
 def get_next_departures(line: int, direction: str, stop: str):
-    # TODO
     """return a dictionary, where keys are hours (current and next), and values are minutes - departures"""
-
-    """staci v tabulke s id SM14 najst td, ktory ma class="next-departure. To je najblizsi cas odchodu.
-    Ja to asi spravim tak, ze ziskam si do dvoch poli (ktore budu predstavovat dve hodiny - aktualnu a dalsiu) vsetky odchody.
-    Potom vyberiem 5 takych, co su rovne, alebo vacsie ako next-departure odchod.
+    """
+    Robim to tak, ze ziskam si do dvoch poli (ktore budu predstavovat dve hodiny - aktualnu a dalsiu) vsetky odchody.
+    Potom vyberiem 6 (konstanta NEXT_SCHEDULES_COUNT) takych, co su rovne, alebo vacsie ako next-departure odchod.
     """
     url = get_line_schedules_url(line, direction, stop)
+    print(url)
     soup = get_soup(url)
 
     now = datetime.now()
     current_hour = now.hour
     current_minute = now.minute
 
-    first_row_td_elements = soup.css.select(f"table[id=SM14] tr[id=sm14T{current_hour}] td")
+    type_of_day = calendar_calculator.calculate_day_for_imhd_schedules()
+    table_id = SCHEDULES_TABLE_IDS_DICT[type_of_day]
+
+    # TODO POZOR toto nebude fungovat pre linku 39 !!! vtedy je tabulka "Pracovne dni" ako SM-21
+    first_row_td_elements = soup.css.select(f"table[id={table_id}] tr[id={table_id.lower()}T{current_hour}] td")
+    first_row_lst = [td.getText() for td in first_row_td_elements if td.getText()]   # TODO oddelime cisla od posledneho charu? A ulozime sem tuples? V tom pripade by sme j mohli nejak zvyraznit
 
     # next_schedules is a dict, where key are hours (current and next), and values are minutes
     next_schedules = {current_hour: []}
 
-    for td in first_row_td_elements:
-        minute = td.getText()
-        if minute:
-            minute = int(minute)
-            if minute >= current_minute:
-                next_schedules[current_hour].append(minute)
+    """ do next_schedules sa ako values davaju stringy. Staci najst index toho, ktory je najblizsie k aktualnemu casu, 
+    a potom vsetky prvky zo zonamu od toho indexu vyssie pridat."""
+
+    for s in first_row_lst:
+        if s:
+            # some departures have specific symbol (route for this line is different)
+            last_char = ''
+            if s[-1].isalpha():
+                last_char = s[-1]
+                s = s[:-1]
+            s = int(s)
+            if s < current_minute:
+                continue
+            else:
+                s = str(s) + last_char
+                next_schedules[current_hour].append(s)
 
     next_schedules_cnt = len(next_schedules[current_hour])   #count of all td elements
     if next_schedules_cnt < NEXT_SCHEDULES_COUNT:
         # let's scrap another row
-        # Take all minutes from next hour. Then add first 6 (or lower) to next_schedules dictionary.
+        # Take all schedules from next hour. Then add first 6 (or lower) to next_schedules dictionary.
         next_hour = current_hour + 1
         second_row_td_elements = soup.css.select(f"table[id=SM14] tr[id=sm14T{next_hour}] td")
-        if second_row_td_elements:
+        second_row_lst = [td.getText() for td in second_row_td_elements if td.getText()]
+        if second_row_lst:
             next_schedules[next_hour] = []
-            while next_schedules_cnt < 6 and second_row_td_elements:
-                td = second_row_td_elements.pop(0)
-                minute = td.getText()
-                if minute:
-                    minute = int(minute)
-                    next_schedules[next_hour].append(minute)
+            while next_schedules_cnt < 6 and second_row_lst:
+                s = second_row_lst.pop(0)
+                if s:
+                    next_schedules[next_hour].append(s)
                     next_schedules_cnt += 1
-    # print(next_schedules)
     return next_schedules
 
 
 if __name__ == "__main__":
-    TESTING = False
-
-    get_next_departures(3, "rača", "Jungmannova")
+    TESTING = True
 
     if TESTING:
-        print("Start of testing.")
-        assert get_line_stops_page_url(3) == "https://imhd.sk/ba/linka/3/bd807c807f847f7f827c82"
-        assert get_line_stops_page_url(9) == "https://imhd.sk/ba/linka/9/bd807c807f847f7f887c88"
-        assert get_line_stops_page_url(33) == "https://imhd.sk/ba/linka/33/bd807c807f847f82827c8282"
-        assert get_line_stops_page_url(42) == "https://imhd.sk/ba/linka/42/bd807c807f847f83817c8381"
-        assert get_line_stops_page_url(83) == "https://imhd.sk/ba/linka/83/bd807c807f847f87827c8782"
-        assert get_line_stops_page_url(84) == "https://imhd.sk/ba/linka/84/bd807c807f847f87837c8783"
+        # print("Start of testing.")
+        # assert get_line_stops_page_url(3) == "https://imhd.sk/ba/linka/3/bd807c807f847f7f827c82"
+        # assert get_line_stops_page_url(9) == "https://imhd.sk/ba/linka/9/bd807c807f847f7f887c88"
+        # assert get_line_stops_page_url(33) == "https://imhd.sk/ba/linka/33/bd807c807f847f82827c8282"
+        # assert get_line_stops_page_url(42) == "https://imhd.sk/ba/linka/42/bd807c807f847f83817c8381"
+        # assert get_line_stops_page_url(83) == "https://imhd.sk/ba/linka/83/bd807c807f847f87827c8782"
+        # assert get_line_stops_page_url(84) == "https://imhd.sk/ba/linka/84/bd807c807f847f87837c8783"
+        #
+        # assert get_line_schedules_url(3, "rača", "Jungmannova") == "https://imhd.sk/ba/cestovny-poriadok/linka/3/Jungmannova/smer-Ra%C4%8Da/bd807c807f847f7f827c8275c18075b680808775be7f75c97f75b3"
+        # assert get_line_schedules_url(3, "rača", "Farského") == "https://imhd.sk/ba/cestovny-poriadok/linka/3/Farsk%C3%A9ho/smer-Ra%C4%8Da/bd807c807f847f7f827c8275c18075b6858775be7f75c98075b3"
+        # assert get_line_schedules_url(84, "DÚBRAVKA, PRI KRÍŽI", "Dvory") == "https://imhd.sk/ba/cestovny-poriadok/linka/84/Dvory/smer-D%C3%BAbravka-Pri-kr%C3%AD%C5%BEi/bd807c807f847f87837c878375c17f75b6858375be7f75c98875b3"
+        # assert get_line_schedules_url(84, "DÚBRAVKA, PRI KRÍŽI", "Hodžovo nám.") == "https://imhd.sk/ba/cestovny-poriadok/linka/84/Hod%C5%BEovo-n%C3%A1m/smer-D%C3%BAbravka-Pri-kr%C3%AD%C5%BEi/bd807c807f847f87837c878375c17f75b6878275be7f75c9808175b3"
+        # assert get_line_schedules_url(31, "CINTORÍN SLÁVIČIE", "Kráľovské údolie") == "https://imhd.sk/ba/cestovny-poriadok/linka/31/Kr%C3%A1%C4%BEovsk%C3%A9-%C3%BAdolie/smer-Cintor%C3%ADn-Sl%C3%A1vi%C4%8Die/bd807c807f847f82807c828075c18075b681848075be7f75c98475b3"
 
-        assert get_line_schedules_url(3, "rača", "Jungmannova") == "https://imhd.sk/ba/cestovny-poriadok/linka/3/Jungmannova/smer-Ra%C4%8Da/bd807c807f847f7f827c8275c18075b680808775be7f75c97f75b3"
-        assert get_line_schedules_url(3, "rača", "Farského") == "https://imhd.sk/ba/cestovny-poriadok/linka/3/Farsk%C3%A9ho/smer-Ra%C4%8Da/bd807c807f847f7f827c8275c18075b6858775be7f75c98075b3"
-        assert get_line_schedules_url(84, "DÚBRAVKA, PRI KRÍŽI", "Dvory") == "https://imhd.sk/ba/cestovny-poriadok/linka/84/Dvory/smer-D%C3%BAbravka-Pri-kr%C3%AD%C5%BEi/bd807c807f847f87837c878375c17f75b6858375be7f75c98875b3"
-        assert get_line_schedules_url(84, "DÚBRAVKA, PRI KRÍŽI", "Hodžovo nám.") == "https://imhd.sk/ba/cestovny-poriadok/linka/84/Hod%C5%BEovo-n%C3%A1m/smer-D%C3%BAbravka-Pri-kr%C3%AD%C5%BEi/bd807c807f847f87837c878375c17f75b6878275be7f75c9808175b3"
-        assert get_line_schedules_url(31, "CINTORÍN SLÁVIČIE", "Kráľovské údolie") == "https://imhd.sk/ba/cestovny-poriadok/linka/31/Kr%C3%A1%C4%BEovsk%C3%A9-%C3%BAdolie/smer-Cintor%C3%ADn-Sl%C3%A1vi%C4%8Die/bd807c807f847f82807c828075c18075b681848075be7f75c98475b3"
+
+        # Note: I cannot test get_next_departures(), because we have different schedules for different days (workdays, school holidays, etc.)
+        # It needs to be tested manually.
+
+        # print(get_next_departures(3, "rača", "Jungmannova"))
+
+        # current_hour = 13
+        # current_minute = 28
+        print(get_next_departures(3, 'rača', 'centrum'))
 
         print("End of testing.")
 
