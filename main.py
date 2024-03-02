e149a7ac356a72b54d63fc117b347dee10d16b93
Daniel <danshaman05@gmail.com>
2024-03-02 16:41:15 +0100

    New Feature: Moja MHD (alpha version)
    Show next schedules for Tram #3

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
 from shmu_scrapper import get_aladin_url_img
+from imhd_scrapper import get_next_departures_from_schedules_table
 
 app = Flask(__name__)
 
@@ -14,3 +15,13 @@ def shmu_radar():
     return render_template("shmu_radar.html")
 
 
+@app.route("/moja-mhd")
+def moja_mhd():
+    data: dict[(int, str), dict[str, list[str]]] = {}
+    # key is a line number, e.g. 3 means tram number 3
+    data[(3, 'rača')] = get_next_departures_from_schedules_table(3, 'rača', 'Jungmannova')
+
+    # data[83] = get_next_departures_from_schedules_table(3, 'rača', 'Jungmannova')
+
+
+    return render_template("moja_mhd.html", data=data)
